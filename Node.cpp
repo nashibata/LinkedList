c7f1da5aabfacfd9f5ea1a2b171de915cbd72474
nashibata <55288319+nashibata@users.noreply.github.com>
2019-12-05 09:02:34 -0800

    Add files via upload

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+#include <cstring>
+#include <vector>
+
+#include "node.h"
+#include "student.h"
+
+using namespace std;
+
+node::Node(student*) {
+  
+}
+
+node* node::getNext(node*) {
+  return next; 
+}
+
+void node::setNext(node*) {
+
+}
+
+student* node::getStudent() {
+  return student;
+}
+
+node::~Node() {
+
+}
